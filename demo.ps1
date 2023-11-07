bd0627b4e3b96cdea4f66166b8fb6c0cb9c2ca20
June Castillote <june.castillote@gmail.com>
2023-11-07 14:58:47 +0800

    Delete demo.ps1

--- demo.ps1
+++
@@ -1,6 +0,0 @@
-#Reference: https://techcommunity.microsoft.com/t5/microsoft-365/get-site-permissions-with-pnp-powershell/m-p/140002
-
-$spoUrl = 'https://ORG.sharepoint.com/sites/SITE'
-$spoConnection = Connect-PnPOnline -Url $spoUrl -Interactive -ReturnConnection
-
-$spoPermissions = .\Get-SpoSitePermissions.ps1 -spoUrl $spoUrl -spoConnection $spoConnection
