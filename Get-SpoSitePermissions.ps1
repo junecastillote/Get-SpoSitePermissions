eb0e9da84a7bc79a615ef7c0c27365df7967f3c2
June Castillote <june.castillote@gmail.com>
2023-11-08 17:49:58 +0800

    Update Get-SpoSitePermissions.ps1

--- Get-SpoSitePermissions.ps1
+++ Get-SpoSitePermissions.ps1
@@ -51,45 +51,44 @@ param (
     [pscredential]
     $Credential,
 
-    [Parameter()]
+    [Parameter(Mandatory)]
     [String]
     $OutCsvFile,
 
     [Parameter()]
-    [switch]
-    $DisplayResults
+    [bool]
+    $DisplayResults = $true
 )
+
+$PSStyle.Progress.View = 'Classic'
+
 if ($OutCsvFile) {
     # if (!(Test-Path $OutCsvFile)) {
     try {
         $null = New-Item -Path $OutCsvFile -ItemType File -Force -ErrorAction Stop
     }
     catch {
-        $_.Exception.Message | Out-Default
+        "    -> [ERROR] : [$($_.Exception.Message)]" | Out-Default
         return $null
     }
     # }
 }
 
-
-
-$PSStyle.Progress.View = 'Classic'
-
 $urlPatternToExclude = ".*-my\.sharepoint\.com/$|.*\.sharepoint\.com/$|.*\.sharepoint\.com/search$|.*\.sharepoint\.com/portals/hub$|.*\.sharepoint\.com/sites/appcatalog$"
 $Url = $Url | Where-Object { $_ -notmatch $urlPatternToExclude } | Sort-Object
 
-$result = [System.Collections.ArrayList]@()
+# $result = [System.Collections.ArrayList]@()
 for ($i = 0; $i -lt $Url.Count; $i++) {
-    # "[$($i+1) of $($Url.Count)] : $url[$i]" | Out-Default
-    [int]$percentComplete = (($i + 1) / ($Url.Count)) * 100
-    Write-Progress -PercentComplete $percentComplete -CurrentOperation "Processing site $($i+1) of $($Url.Count) ($percentComplete%)" -Activity "SharePoint Site Permission" -Status $($url[$i])
+    "[$($i+1) of $($Url.Count)] : $($url[$i])" | Out-Default
+    # $percentComplete = (($i + 1) / ($Url.Count)) * 100
+    # Write-Progress -PercentComplete $percentComplete -CurrentOperation "Processing site $($i+1) of $($Url.Count) ($percentComplete%)" -Activity "Get SharePoint Site Permission" -Status $($url[$i])
 
     try {
         Connect-PnPOnline -Url $url[$i] -Credentials $Credential -ErrorAction Stop
         $site = Get-PnPTenantSite -Identity $url[$i] -ErrorAction Stop
     }
     catch {
-        $_.Exception.Message | Out-Default
+        "    -> [ERROR] : [$($_.Exception.Message)]" | Out-Default
         Continue # Skip to next item
     }
 
@@ -100,38 +99,52 @@ for ($i = 0; $i -lt $Url.Count; $i++) {
 
     $members = [System.Collections.ArrayList]@()
 
-    $web = Get-PnPWeb -Includes RoleAssignments
+    try {
+        $web = Get-PnPWeb -Includes RoleAssignments -ErrorAction Stop
+    }
+    catch {
+        "    -> [ERROR] : [$($_.Exception.Message)]" | Out-Default
+        Continue
+
+    }
     $context = Get-PnPContext
     foreach ($ra in $web.RoleAssignments) {
-        $context.Load($ra.RoleDefinitionBindings)
-        $context.Load($ra.Member)
-        $context.ExecuteQuery()
+        try {
+            $context.Load($ra.RoleDefinitionBindings)
+            $context.Load($ra.Member)
+            $context.ExecuteQuery()
 
-        $null = $members.Add(
-            $(
-                [PSCustomObject]$(
-                    [ordered]@{
-                        Name          = $ra.Member.Title
-                        LoginName     = $(
-                            if ($ra.Member.LoginName -like "*guest#*") {
-                                ($ra.Member.LoginName).Split('#')[-1]
-                            }
-                            else {
-                                ($ra.Member.LoginName).Split('|')[-1]
-                            }
-                        )
-                        Permission    = $($ra.RoleDefinitionBindings[0].Name.ToString())
-                        PrincipalType = $ra.Member.TypedObject.ToString().Split('.')[-1]
-                    }
+            $null = $members.Add(
+                $(
+                    [PSCustomObject]$(
+                        [ordered]@{
+                            Name          = $ra.Member.Title
+                            LoginName     = $(
+                                if ($ra.Member.LoginName -like "*guest#*") {
+                                    ($ra.Member.LoginName).Split('#')[-1]
+                                }
+                                else {
+                                    ($ra.Member.LoginName).Split('|')[-1]
+                                }
+                            )
+                            Permission    = $($ra.RoleDefinitionBindings[0].Name.ToString())
+                            PrincipalType = $ra.Member.TypedObject.ToString().Split('.')[-1]
+                        }
+                    )
                 )
             )
-        )
+        }
+        catch {
+            "    -> [ERROR] : [$($_.Exception.Message)]" | Out-Default
+            Continue
+        }
     }
 
     foreach ($item in $members) {
         if ($item.PrincipalType -eq 'User') {
             $tempResult = $(
-                [PSCustomObject]$([ordered]@{
+                [PSCustomObject]$(
+                    [ordered]@{
                         SiteName        = $web.Title
                         SiteURL         = $web.Url
                         SharePointGroup = $null
@@ -148,10 +161,13 @@ for ($i = 0; $i -lt $Url.Count; $i++) {
                             }
                         )
 
-                    })
+                    }
+                )
             )
-            $null = $result.Add($tempResult)
-            $tempResult | Export-Csv -Path $OutCsvFile -NoTypeInformation -Append -Force -Encoding unicode -Delimiter "`t"
+            # $null = $result.Add($tempResult)
+            if ($OutCsvFile) {
+                $tempResult | Export-Csv -Path $OutCsvFile -NoTypeInformation -Append -Force -Encoding unicode -Delimiter "`t"
+            }
             if ($DisplayResults) { $tempResult }
         }
 
@@ -186,16 +202,18 @@ for ($i = 0; $i -lt $Url.Count; $i++) {
                         }
                     )
                 )
-                $null = $result.Add($tempResult)
-                $tempResult | Export-Csv -Path $OutCsvFile -NoTypeInformation -Append -Force -Encoding unicode -Delimiter "`t"
+                # $null = $result.Add($tempResult)
+                if ($OutCsvFile) {
+                    $tempResult | Export-Csv -Path $OutCsvFile -NoTypeInformation -Append -Force -Encoding unicode -Delimiter "`t"
+                }
                 if ($DisplayResults) { $tempResult }
             }
         }
     }
 }
 
+# Write-Progress -PercentComplete 100 -Completed -Activity "Get SharePoint Site Permission"
+
 if ($OutCsvFile) {
     "Results are exported to $(Resolve-Path $OutCsvFile)." | Out-Default
 }
-
-
